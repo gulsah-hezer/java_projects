4e2067dbabf052a62a82ee3d725c4509f2ba16f7
gulsah-hezer <66849531+gulsah-hezer@users.noreply.github.com>
2020-08-27 13:39:07 +0300

    Update Flappy_Bird.java

--- Flappy_Bird.java
+++ Flappy_Bird.java
@@ -6,29 +6,93 @@
 /*There is extensive use of java swing classes and of course OOPs concepts.*/
 
 package FlappyBird;
-import javax.swing.Jframe;
-İMPORT
 
+import javax.swing.Jframe;
+import java.util.Timer;
+import java.awt.Graphics;
 
 public class FlappyBird
 {
 
   public static FlappyBird flappyBird;
   
-  public FlappyBird()
-  {
-    Jframe jframe=new Jframe();
-    
-    public final int WIDTH=800, HEIGHT=800;
-    jframe.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE);
-    jframe.setSize(WIDTH, HEIGHT);
-    jframe.setVisible(true);
-    
-    
-  }
+  public Renderer renderer;
+  
+  public Rectangle bird;
+  
+  public final int WIDTH=800, HEIGHT=800;
+  
+  public ArrayList<Rectangle> columns;
+  
+  public Random rand;
   
   
   
+        /*Constructor*/
+        public FlappyBird()
+        {
+          Jframe jframe = new Jframe(); 
+          Timer timer = new Timer();
+
+          renderer= new Renderer();
+
+          jframe=add(renderer);
+
+          jframe.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE);
+          jframe.setSize(WIDTH, HEIGHT);
+          jframe.setVisible(true);
+          bird = new Rectangle(WIDTH/2 -10, HEIGHT/2 -10, 20, 20);
+          timer.start();
+          
+          columns= new ArrayList<Rectangle>();
+
+        }
+
+        public void addColumn()
+        {
+           int space= 300 ;
+           int width= 100 ;
+           int height= 50 + rand.nextInt(300);
+          
+          columns.add( new Rectangle(WIDTH+ width+  columns.size()*300, height));
+          
+          
+        }
+        
+  
+        public void paintColumn(Graphics g, Rectangle column)
+        {
+           g.setColor(Color.green.darker());
+           g.fillRect(column.x, column.y, column.width, column.height);
+          
+        
+        }
+ 
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {  
+         renderer.repaint();
+        }
+  
+  
+        public void repaint(Graphics g)
+        {
+          g.setColor(Color.cyan);
+          g.fillRect(0,0,WIDTH, HEIGHT);
+          
+          g.setColor(Color.orange);
+          g.fillRect(0,HEIGHT-150, WIDTH, 150);
+          
+          g.setColor(Color.green);
+          g.fillRect(0,HEIGHT-150, WIDTH, 20);
+         
+          g.setColor(Color.red);
+          g.fillRect(bird.x,bird.y, bird.width, bird.height);
+          
+        }
+
+  
+ 
   
   
   public static void main(String[] args)
